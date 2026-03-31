afb4efabaaa5379cf2f49f52cb50b8c72df098fd
Claus Höfele <claus@claushoefele.com>
2026-03-31 16:36:40 +0200

    Start nav bar

---
+++ src/NavBar.tsx
@@ -0,0 +1,16 @@
+import { CircleUser } from "lucide-react";
+
+function NavBar() {
+  return (
+    <div className="grid grid-cols-[1fr_auto_1fr] items-center">
+      <p>CineLight</p>
+      <li className="flex gap-10">
+        <ul>Discover 123</ul>
+        <ul>My Favourites</ul>
+      </li>
+      <CircleUser className="justify-self-end" />
+    </div>
+  );
+}
+
+export default NavBar;
