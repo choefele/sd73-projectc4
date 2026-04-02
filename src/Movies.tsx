99ffae89ca6533ba644ac3a65bc1af94bbcc73b5
Claus Höfele <claus@claushoefele.com>
2026-04-02 13:30:08 +0200

    Show popular movies

--- src/Movies.tsx
+++ src/Movies.tsx
@@ -1,22 +1,60 @@
+import { useEffect, useState } from "react";
 import MoviesCard from "./MoviesCard";
+import { fetchPopularMovies, type TmdbPopularMoviesResponse } from "./tmdbapi";
 
 function Movies() {
+  const [moviesResponse, setMoviesResponse] =
+    useState<TmdbPopularMoviesResponse>();
+  const [error, setError] = useState<string>();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    let ignore = false;
+
+    const fetchData = async () => {
+      setLoading(true);
+
+      try {
+        const moviesResponse = await fetchPopularMovies();
+        if (!ignore) {
+          setMoviesResponse(moviesResponse);
+        }
+      } catch (err) {
+        if (!ignore) {
+          setError(err instanceof Error ? err.message : "Unknown error");
+        }
+      } finally {
+        if (!ignore) {
+          setLoading(false);
+        }
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
   return (
     <>
       <h2>Movies</h2>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
-        <MoviesCard title="Title" year="1234" genre="Genre" rating={5.3} />
-        <MoviesCard title="Title" year="1234" genre="Genre" rating={5.3} />
-        <MoviesCard title="Title" year="1234" genre="Genre" rating={5.3} />
-        <MoviesCard title="Title" year="1234" genre="Genre" rating={5.3} />
-        <MoviesCard title="Title" year="1234" genre="Genre" rating={5.3} />
-        <MoviesCard title="Title" year="1234" genre="Genre" rating={5.3} />
-        <MoviesCard title="Title" year="1234" genre="Genre" rating={5.3} />
-        <MoviesCard title="Title" year="1234" genre="Genre" rating={5.3} />
-        <MoviesCard title="Title" year="1234" genre="Genre" rating={5.3} />
-        <MoviesCard title="Title" year="1234" genre="Genre" rating={5.3} />
-        <MoviesCard title="Title" year="1234" genre="Genre" rating={5.3} />
-      </div>
+      {loading && <p>Loading..</p>}
+      {error && <p>{error}</p>}
+      {moviesResponse && (
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
+          {moviesResponse.results.map((movie) => (
+            <MoviesCard
+              key={movie.id}
+              title={movie.title}
+              year={movie.release_date}
+              genre="Genre"
+              rating={movie.vote_average}
+            />
+          ))}
+        </div>
+      )}
     </>
   );
 }
